a648fa1c275fe68a8048fb7fa50efb5d1a9456fa
wyl <1513078461@qq.com>
2022-04-17 21:38:23 +0800

    format

--- src/components/LoginPlugin.vue
+++ src/components/LoginPlugin.vue
@@ -1,84 +1,105 @@
 <template>
   <div class="wHeader">
-      <div style="float: right; padding-top: 6px">
+    <div style="float: right; padding-top: 6px">
+      <el-button
+        style="float: right"
+        type="primary"
+        @click="dialogVisible = true"
+        v-if="this.$store.state.isLogin === false"
+        >登录</el-button
+      >
+      <el-button
+        style="float: right"
+        type="primary"
+        @click="handleLogout"
+        v-if="this.$store.state.isLogin === true"
+        >登出</el-button
+      >
+    </div>
+    <el-dialog title="" :visible.sync="dialogVisible" width="600px">
+      <h1>登录</h1>
+      <div style="margin-top: 15px">
+        <el-input v-model="username" placeholder="用户名"></el-input>
+      </div>
+      <div style="margin-top: 15px">
+        <el-input
+          v-model="password"
+          placeholder="密码"
+          show-password
+        ></el-input>
+      </div>
+      <div style="margin-top: 10px">
+        <!-- <a style=" color:#409EFF; float:right;margin-right:15px; text-decoration:none" href="">忘记密码</a> -->
         <el-button
-          style="float: right"
-          type="primary"
-          @click="dialogVisible = true"
-          v-if="this.$store.state.isLogin === false"
-          >登录</el-button
-        >
-        <el-button
-          style="float: right"
-          type="primary"
-          @click="handleLogout"
-          v-if="this.$store.state.isLogin === true"
-          >登出</el-button
+          type="text"
+          @click="makeDialogUnvisAndDialog2vis"
+          style="float: right; margin-right: 0px"
+          >忘记密码</el-button
         >
       </div>
-      <el-dialog title="" :visible.sync="dialogVisible" width="600px" >
-        <h1>登录</h1>
-        <div style="margin-top: 15px">
-          <el-input v-model="username" placeholder="用户名"></el-input>
-        </div>
-        <div style="margin-top: 15px">
-          <el-input
-            v-model="password"
-            placeholder="密码"
-            show-password
-          ></el-input>
-        </div>
-        <div style="margin-top: 10px " >
-            <!-- <a style=" color:#409EFF; float:right;margin-right:15px; text-decoration:none" href="">忘记密码</a> -->
-            <el-button type="text" @click="makeDialogUnvisAndDialog2vis" style="float:right;margin-right:0px;">忘记密码</el-button>
-        </div>
 
-        <span slot="footer" class="dialog-footer" style=":text-align:center">
-          <!-- <el-button @click="dialogVisible = false">取 消</el-button> -->
-          <el-button type="primary" @click="handleLogin" style=":text-align:center">确 定</el-button>
-          <span style=":text-align:center;line-height:30px">
-            <li style="font-size: 14px">没有账号？
-              <!-- <a style="color:#409EFF; text-decoration:none" @click="makeDialogUnvisAndDialog2vis">立即注册</a> -->
-              <el-button type="text" @click="makeDialogUnvisAndDialog2vis">立即注册</el-button>
-            </li>
-          </span>
+      <span slot="footer" class="dialog-footer" style=":text-align: center">
+        <!-- <el-button @click="dialogVisible = false">取 消</el-button> -->
+        <el-button
+          type="primary"
+          @click="handleLogin"
+          style=":text-align: center"
+          >确 定</el-button
+        >
+        <span style=":text-align: center; line-height: 30px">
+          <li style="font-size: 14px">
+            没有账号？
+            <!-- <a style="color:#409EFF; text-decoration:none" @click="makeDialogUnvisAndDialog2vis">立即注册</a> -->
+            <el-button type="text" @click="makeDialogUnvisAndDialog2vis"
+              >立即注册</el-button
+            >
+          </li>
         </span>
-      </el-dialog>
-      
-      <el-dialog title="" :visible.sync="dialog2Visible" width="600px" >
-        <h1>注册</h1>
-        <div style="margin-top: 15px">
-          <el-input v-model="username" placeholder="用户名"></el-input>
-        </div>
-        <div style="margin-top: 15px">
-          <el-input v-model="username" placeholder="手机号码"></el-input>
-        </div>
-        <div style="margin-top: 15px">
-          <el-input
-            v-model="password"
-            placeholder="密码"
-            show-password
-          ></el-input>
-        </div>
-        <div style="margin-top: 15px">
-          <el-input
-            v-model="password"
-            placeholder="确认密码"
-            show-password
-          ></el-input>
-        </div>
+      </span>
+    </el-dialog>
 
-        <span slot="footer" class="dialog-footer" style=":text-align:center">
-          <!-- <el-button @click="dialogVisible = false">取 消</el-button> -->
-          <el-button type="primary" @click="handleLogin" style=":text-align: center">注 册</el-button>
-          <span style=":text-align:center;line-height:30px">
-            <li style="font-size: 14px">已有账号？
-              <!-- <a style="color:#409EFF; text-decoration:none" @click="makeDialog2UnvisAndDialogvis">立即登录</a> -->
-              <el-button type="text" @click="makeDialogUnvisAndDialog2vis">立即登录</el-button>
-            </li>
-          </span>
+    <el-dialog title="" :visible.sync="dialog2Visible" width="600px">
+      <h1>注册</h1>
+      <div style="margin-top: 15px">
+        <el-input v-model="username" placeholder="用户名"></el-input>
+      </div>
+      <div style="margin-top: 15px">
+        <el-input v-model="username" placeholder="手机号码"></el-input>
+      </div>
+      <div style="margin-top: 15px">
+        <el-input
+          v-model="password"
+          placeholder="密码"
+          show-password
+        ></el-input>
+      </div>
+      <div style="margin-top: 15px">
+        <el-input
+          v-model="password"
+          placeholder="确认密码"
+          show-password
+        ></el-input>
+      </div>
+
+      <span slot="footer" class="dialog-footer" style=":text-align: center">
+        <!-- <el-button @click="dialogVisible = false">取 消</el-button> -->
+        <el-button
+          type="primary"
+          @click="handleLogin"
+          style=":text-align: center"
+          >注 册</el-button
+        >
+        <span style=":text-align: center; line-height: 30px">
+          <li style="font-size: 14px">
+            已有账号？
+            <!-- <a style="color:#409EFF; text-decoration:none" @click="makeDialog2UnvisAndDialogvis">立即登录</a> -->
+            <el-button type="text" @click="makeDialogUnvisAndDialog2vis"
+              >立即登录</el-button
+            >
+          </li>
         </span>
-      </el-dialog>
+      </span>
+    </el-dialog>
   </div>
 </template>
 
@@ -163,15 +184,14 @@ export default {
             nt({
               title: "注册成功",
               type: "success",
-              position: 'top-left',
-              offset:60,
+              position: "top-left",
+              offset: 60,
             });
-            
           } else {
             nt.error({
               title: "密码不同",
-              position: 'top-left',
-              offset:60
+              position: "top-left",
+              offset: 60,
             });
           }
         })
@@ -189,7 +209,7 @@ export default {
     makeDialog2UnvisAndDialogvis() {
       this.dialogVisible = true;
       this.dialog2Visible = false;
-    }
+    },
   },
 };
 </script>
